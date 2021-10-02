64c6cc35ae666dfb84aa4a65de3e21f3c277bb7e
Alex Irkhin <workirhin@yandex.ru>
2021-10-02 16:06:14 +0300

    Added catch error(end)

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import "regenerator-runtime/runtime";
@@ -9,6 +9,7 @@ import { save } from "../../store/charactersSlice";
 
 // Components
 import { Button } from "../UI";
+import { ErrorSearch } from "../ErrorComponents";
 
 // Api
 import api from "../../api";
@@ -17,7 +18,7 @@ import api from "../../api";
 import { createArrPages } from "../../utils";
 
 // Images
-import { iconNext, iconPrev } from "../../images"
+import { iconNext, iconPrev } from "../../images";
 
 const Container = styled.div`
   display: flex;
@@ -29,92 +30,119 @@ const Container = styled.div`
 `;
 
 const Img = styled.img`
-    width: 10px;
-    height: 10px;
-    margin: 0;
-    padding: 0;
-`
+  width: 10px;
+  height: 10px;
+  margin: 0;
+  padding: 0;
+`;
 
 const Pagination = () => {
   const { next, prev, pages, currentPage, queryString } = useSelector(
     (state) => state.pagination
   );
   const dispatch = useDispatch();
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleChangePage = (e) => {
     const value = +e.target.value;
-    api.getCharacterByPage(value, queryString).then((res) => {
-      const { info, results } = res;
-      dispatch(change({ ...info, currentPage: value }));
-      dispatch(save(results));
-    });
+    api
+      .getCharacterByPage(value, queryString)
+      .then((res) => {
+        const { info, results } = res;
+        dispatch(change({ ...info, currentPage: value }));
+        dispatch(save(results));
+      })
+      .catch((e) => {
+        setErrorMessage(e.message);
+      });
   };
 
   const handlePrev = () => {
     const value = currentPage - 1;
-    api.getCharacterByPage(value, queryString).then((res) => {
-      const { info, results } = res;
-      dispatch(change({ ...info, currentPage: value }));
-      dispatch(save(results));
-    });
+    api
+      .getCharacterByPage(value, queryString)
+      .then((res) => {
+        const { info, results } = res;
+        dispatch(change({ ...info, currentPage: value }));
+        dispatch(save(results));
+      })
+      .catch((e) => {
+        setErrorMessage(e.message);
+      });
   };
 
   const handleNext = () => {
     const value = currentPage + 1;
-    api.getCharacterByPage(value, queryString).then((res) => {
-      const { info, results } = res;
-      dispatch(change({ ...info, currentPage: value }));
-      dispatch(save(results));
-    });
+    api
+      .getCharacterByPage(value, queryString)
+      .then((res) => {
+        const { info, results } = res;
+        dispatch(change({ ...info, currentPage: value }));
+        dispatch(save(results));
+      })
+      .catch((e) => {
+        setErrorMessage(e.message);
+      });
+  };
+
+  const handleCloseError = () => {
+    setError(false);
+    setErrorMessage("");
   };
 
   const arrPages = createArrPages(pages, currentPage);
 
   return (
-    <Container>
-      <Button theme="button-link" onClick={handlePrev} disabled={!prev}>
-        <Img src={`.${iconPrev}`} alt="prev"/>
-      </Button>
-      {currentPage > 3 ? (
-        <>
-          <Button
-            theme="button-link"
-            value={1}
-            onClick={(e) => handleChangePage(e)}
-          >
-            1
-          </Button>
-          {"..."}
-        </>
-      ) : null}
-      {arrPages.map((num) => (
-        <Button
-          value={num}
-          theme="button-link"
-          key={num}
-          active={currentPage === num}
-          onClick={(e) => handleChangePage(e)}
-          disabled={currentPage === num}
-        >
-          {num}
+    <>
+      <Container>
+        <Button theme="button-link" onClick={handlePrev} disabled={!prev}>
+          <Img src={`.${iconPrev}`} alt="prev" />
         </Button>
-      ))}
-      {currentPage <= pages - 3 ? (
-        <>
-          {"..."}
+        {currentPage > 3 ? (
+          <>
+            <Button
+              theme="button-link"
+              value={1}
+              onClick={(e) => handleChangePage(e)}
+            >
+              1
+            </Button>
+            {"..."}
+          </>
+        ) : null}
+        {arrPages.map((num) => (
           <Button
+            value={num}
             theme="button-link"
-            value={pages}
+            key={num}
+            active={currentPage === num}
             onClick={(e) => handleChangePage(e)}
+            disabled={currentPage === num}
           >
-            {pages}
+            {num}
           </Button>
-        </>
+        ))}
+        {currentPage <= pages - 3 ? (
+          <>
+            {"..."}
+            <Button
+              theme="button-link"
+              value={pages}
+              onClick={(e) => handleChangePage(e)}
+            >
+              {pages}
+            </Button>
+          </>
+        ) : null}
+        <Button theme="button-link" onClick={handleNext} disabled={!next}>
+          <Img src={`.${iconNext}`} alt="next" />
+        </Button>
+      </Container>
+      {error ? (
+        <ErrorSearch text={errorMessage} onClose={handleCloseError} />
       ) : null}
-      <Button theme="button-link" onClick={handleNext} disabled={!next}>
-        <Img src={`.${iconNext}`} alt="next"/>
-      </Button>
-    </Container>
+    </>
   );
 };
 
